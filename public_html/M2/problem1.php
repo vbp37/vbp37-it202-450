939301b5708d9a19543d5e9ac024ac165aabfeff
vbp37 <vbp37@njit.edu>
2023-05-31 12:14:01 -0400

    removing the array before the processArray function to see if it makes a difference

--- public_html/M2/problem1.php
+++ public_html/M2/problem1.php
@@ -3,8 +3,9 @@ $a1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 $a2 = [0, 1, 3, 5, 7, 9, 2, 4, 6, 8, 10];
 $a3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 $a4 = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
-$arr = [$a1, $a2, $a3, $a4];
+//$arr = [$a1, $a2, $a3, $a4];
 function processArray($arr) {
+    
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Odds output:<br>";
     foreach ($arr as  $value)
