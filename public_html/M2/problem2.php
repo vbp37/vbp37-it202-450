279305267ec1a3975f82a92b7ff4fedcaf88556e
vbp37 <vbp37@njit.edu>
2023-06-01 16:37:46 -0400

    using number_format to round instead of round()

--- public_html/M2/problem2.php
+++ public_html/M2/problem2.php
@@ -13,7 +13,7 @@ function getTotal($arr) {
 
     }
     
-    $roundedTotal = round($total, 2);
+    $roundedTotal = number_format($total, 2);
     //TODO do adding here
     //TODO do rounding stuff here
     echo "The total is " . $roundedTotal . "<br>";
