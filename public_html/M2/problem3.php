9c831d60d864ff3d968c265242c62085d022446c
vbp37 <vbp37@njit.edu>
2023-06-01 17:32:12 -0400

    trying to fix data types

--- public_html/M2/problem3.php
+++ public_html/M2/problem3.php
@@ -31,7 +31,7 @@ function bePositive($arr) {
       //  }
        //  echo var_dump($value) . "<br>";
        echo $NewValue . " " ;
-       echo var_dump($NewValue) . "<br>";
+       echo var_dump($value) . "<br>";
     }
 
     //TODO use echo to output all of the values as positive (even if they were originally positive) 
