622bcb1099f677ab6b32476d1dc3329ff04fc53e
vbp37 <vbp37@njit.edu>
2023-06-01 16:42:51 -0400

    using round instead of number format to see if it changes anything

--- public_html/M2/problem2.php
+++ public_html/M2/problem2.php
@@ -13,10 +13,10 @@ function getTotal($arr) {
 
     }
     
-    $roundedTotal = number_format($total, 2);
+    $roundedTotal = round($total, 2);
     //TODO do adding here
     //TODO do rounding stuff here
-    echo "The total is " . number_format($roundedTotal,2) . "<br>";
+    echo "The total is " . round($roundedTotal,2) . "<br>";
 }
 echo "Problem 2: Adding Floats<br>";
 ?>
