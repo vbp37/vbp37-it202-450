757badba88997a3e3d90fcba45e5601c56172a21
vbp37 <vbp37@njit.edu>
2023-08-01 19:06:46 -0400

    created a Joke_profile.php which is supposed to allow the admin to remove jokes from database, howver database is currently empty

--- partials/nav.php
+++ partials/nav.php
@@ -47,6 +47,7 @@ session_start();
             <li><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
             <li><a href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
             <li><a href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
+            <li><a href="<?php echo get_url('admin/joke_profile.php'); ?>">Edit API Jokes</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
