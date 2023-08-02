8dc5f4ed8a2e7b48dd38e36b754d7b285d7391f1
vbp37 <vbp37@njit.edu>
2023-08-02 16:50:21 -0400

    connected userjokes to DadJokes table

--- partials/nav.php
+++ partials/nav.php
@@ -37,6 +37,8 @@ session_start();
             <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
             <li><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
             <li><a href="<?php echo get_url('dadjoke.php'); ?>">Joke Generator</a></li> 
+            <li><a href="<?php echo get_url('create_joke.php'); ?>">Create Joke</a></li> 
+
 
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
